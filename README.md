c7d00548f0f6443d30c6a0e482f468d0b9a71993
Cyrus Orbell <orbell.cyrus@gmail.com>
2026-03-02 23:39:37 -0600

    fixed things

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # RecipeWebsite
+Run in terminall: 
--- db/connection.js
+++ db/connection.js
@@ -2,10 +2,10 @@ const mysql = require('mysql2');
 require('dotenv').config();
 
 const connection = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  password: 'Howdididoit6789',
-  database: 'RecipeWebsite'
+  host: process.env.DB_HOST,      
+  user: process.env.DB_USER,     
+  password: process.env.DB_PASSWORD,  
+  database: process.env.DB_NAME 
 });
 
 connection.connect((err) => {
